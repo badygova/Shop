006d8459e3c6e98a8acfc77f7e325c70547ad569
liya19 <liya1999.19@yandex.ru>
2019-05-12 20:19:18 +0300

    added style from form

--- views/user/login.php
+++ views/user/login.php
@@ -1,33 +1,46 @@
+<!-- Stylesheets -->
+<link rel="stylesheet" href="../../template/css/bootstrap.min.css"/>
+<link rel="stylesheet" href="../../template/css/font-awesome.min.css"/>
+<link rel="stylesheet" href="../../template/css/flaticon.css"/>
+<link rel="stylesheet" href="../../template/css/slicknav.min.css"/>
+<link rel="stylesheet" href="../../template/css/jquery-ui.min.css"/>
+<link rel="stylesheet" href="../../template/css/owl.carousel.min.css"/>
+<link rel="stylesheet" href="../../template/css/animate.css"/>
+<link rel="stylesheet" href="../../template/css/style.css"/>
+<link rel="stylesheet" href="../../template/css/form.css"/>
 
+<section class="login">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4 col-sm-offset-4 padding-right">
 
-    <section>
-        <div class="container">
-            <div class="row">
-
-                <div class="col-sm-4 col-sm-offset-4 padding-right">
-
-                    <?php if (isset($errors) && is_array($errors)): ?>
-                        <ul>
-                            <?php foreach ($errors as $error): ?>
-                                <li> - <?php echo $error; ?></li>
-                            <?php endforeach; ?>
-                        </ul>
-                    <?php endif; ?>
-
+                <?php if (isset($errors) && is_array($errors)): ?>
+                    <ul>
+                        <?php foreach ($errors as $error): ?>
+                            <li style="font-size: 21px; color: white"> - <?php echo $error; ?></li>
+                        <?php endforeach; ?>
+                    </ul>
+                <?php endif; ?>
+                <div class="test">
                     <div class="signup-form"><!--sign up form-->
-                        <h2>Вход на сайт</h2>
-                        <form action="#" method="post">
+                        <h2 class="loginH">Вход на сайт</h2><br>
+                        <form action="login.php" class="form_login" method="post">
                             <input type="email" name="email" placeholder="E-mail" value="<?php echo $email; ?>"/>
-                            <input type="password" name="password" placeholder="Пароль" value="<?php echo $password; ?>"/>
-                            <input type="submit" name="submit" class="btn btn-default" value="Вход" />
+                            <input type="password" name="password" placeholder="Пароль"
+                                   value="<?php echo $password; ?>"/>
+                            <input type="submit" name="submit" class="btn btn-default" style="background-color: white; margin-top: 7px" value="Вход"/>
                         </form>
+                        <a href="http://localhost/user/register"><input type="submit" name="submit"
+                                                                        class="btn btn-default"
+                                                                        style="background-color: white; width: 287px;
+                                                                         margin-left: 31px"
+                                                                        value="Регистрация"/></a>
                     </div><!--/sign up form-->
-
-
-                    <br/>
-                    <br/>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+    </div>
+</section>
+
 
--- views/user/register.php
+++ views/user/register.php
@@ -1,6 +1,15 @@
+<!-- Stylesheets -->
+<link rel="stylesheet" href="../template/css/bootstrap.min.css"/>
+<link rel="stylesheet" href="../template/css/font-awesome.min.css"/>
+<link rel="stylesheet" href="../template/css/flaticon.css"/>
+<link rel="stylesheet" href="../template/css/slicknav.min.css"/>
+<link rel="stylesheet" href="../template/css/jquery-ui.min.css"/>
+<link rel="stylesheet" href="../template/css/owl.carousel.min.css"/>
+<link rel="stylesheet" href="../template/css/animate.css"/>
+<link rel="stylesheet" href="../template/css/style.css"/>
+<link rel="stylesheet" href="../template/css/form.css"/>
 
-
-    <section>
+    <section class="login">
         <div class="container">
             <div class="row">
 
@@ -16,17 +25,18 @@
                                 <?php endforeach; ?>
                             </ul>
                         <?php endif; ?>
-
+                    <div class="test">
                         <div><!--sign up form-->
-                            <h2>Регистрация на сайте</h2>
+                            <h2 style="color: white; text-align: center; margin-bottom: 10px">Регистрация на сайте</h2>
                             <form action="" method="post">
                                 <input type="text" name="name" placeholder="Имя" value="<?php echo $name; ?>"/>
                                 <input type="email" name="email" placeholder="E-mail" value="<?php echo $email; ?>"/>
                                 <input type="password" name="password" placeholder="Пароль" value="<?php echo $password; ?>"/>
-                                <input type="submit" name="submit" class="btn btn-default" value="Регистрация" />
+                                <input type="submit" name="submit" class="btn btn-default" value="Регистрация" style="background-color: white;
+margin-top: 7px"/>
                             </form>
                         </div><!--/sign up form-->
-
+                    </div>
                     <?php endif; ?>
                     <br/>
                     <br/>
